630f5dbe506e9d6c26ae8de53933bbabf1b0e9e8
talset <talset55@gmail.com>
2025-03-12 21:56:25 +0100

    aws: fix startup script

--- extra/startup.sh
+++ extra/startup.sh
@@ -268,6 +268,7 @@ use_endpoint_heuristics = True' > /etc/boto.cfg
         cd /tmp/ssm
         wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
         dpkg -i amazon-ssm-agent.deb
+        cd -
         systemctl enable amazon-ssm-agent
         systemctl start amazon-ssm-agent
 
